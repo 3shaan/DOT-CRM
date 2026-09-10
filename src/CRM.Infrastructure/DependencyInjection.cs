ca88c9c61951b1a77c0b8fcbef70d0349fb7996c
Eshan <3shaan.dev@proton.me>
2026-09-11 01:15:16 +0600

    feat: add Dependency Injection for infrastructure services and PostgreSQL configuration

---
+++ src/CRM.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,58 @@
+using CRM.Application.Auth.Common.Interface;
+using CRM.Application.Common.Interface;
+using CRM.Infrastructure.Identity;
+using CRM.Infrastructure.Persistence;
+using CRM.Infrastructure.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CRM.Infrastructure;
+
+public static class DependencyInjection
+{
+
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+        // connect to postgresql 
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseNpgsql(connectionString));
+
+
+        services.AddIdentityCore<ApplicationUser>(options =>
+        {
+            options.Password.RequireDigit = true;
+            options.Password.RequireLowercase = true;
+            options.Password.RequireUppercase = true;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequiredLength = 8;
+
+            options.Lockout.MaxFailedAccessAttempts = 5;
+            options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(15); // Lockout for 15 minutes after 5 failed attempts
+        })
+        .AddRoles<IdentityRole<Guid>>()
+        .AddEntityFrameworkStores<ApplicationDbContext>().AddSignInManager()
+            .AddDefaultTokenProviders();
+
+
+        //jwt
+
+        services.Configure<JwtOptions>(configuration.GetSection(JwtOptions.SectionName));
+
+
+        //auth service
+
+        services.AddScoped<IAuthService, AuthService>();
+
+        // jwt token service
+        services.AddScoped<IJwtTokenService, JwtTokenService>();
+
+
+        return services;
+    }
+
+}
